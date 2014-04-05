e27ff2a596f96bc838ceea8240c7038fb9278246
mgascal <mgascal@advisen.com>
2014-04-05 09:51:07 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestGitRails::Application.config.secret_key_base = 'b0aadaeb7a7dec634df51242b0da16e0175ee37eafe5f93f701c485faff782c8e4e1b84a3c1084a8932aa68c757d1a92158e4c9badd18de0970c27020c3e4c23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestGitRails::Application.config.session_store :cookie_store, key: '_testGitRails_session'
